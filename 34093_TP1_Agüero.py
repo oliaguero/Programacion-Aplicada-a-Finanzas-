261491a833271a49593c0dfa0402645771324565
Olivia Agüero <102388241+oliaguero@users.noreply.github.com>
2023-04-06 12:52:29 -0300

    Add files via upload

--- 34093_TP1_Agüero.py
+++ 34093_TP1_Agüero.py
@@ -22,6 +22,8 @@ class myarray1:
         pos = 0
         fila = 1
         col = 1
+        if j > self.r or k > self.c:
+            raise ValueError("Las coordenadas j y k deben ser menores que r y c respectivamente.")
         if self.by_row == True:
             for n in elems: 
                 if fila == j and col == k:
@@ -50,6 +52,8 @@ class myarray1:
         pos = 0
         fila = 1
         col = 1
+        if m >= self.r * self.c:
+            raise ValueError("La posición m debe ser menor que r * c.")
         if self.by_row == True:
             for n in elems:
                 if pos == m:
@@ -76,6 +80,8 @@ class myarray1:
             
         elems = self.lista 
         new_list = []
+        if self.r <= 0 or self.c <= 0:
+          raise ValueError("La matriz debe tener al menos una fila y una columna.")
         if self.by_row == True:
             for fila in range(self.r):
                 for columna in range (self.c):
@@ -84,6 +90,7 @@ class myarray1:
             for columna in range(self.c):
                 for fila in range (self.r):
                     new_list.append (elems[fila * self.c + columna])
+    
         return myarray1 (new_list,self.r,self.c,self.by_row)
         
     def get_row (self,j):
@@ -94,6 +101,8 @@ class myarray1:
         fila = 1
         col = 1
         l_row = []
+        if j > self.r:
+           raise ValueError("La fila j debe ser menor que r.")
         if self.by_row == True:
             for n in elems: 
                 if fila == j:
@@ -121,6 +130,8 @@ class myarray1:
         fila = 1
         col = 1
         l_col = []
+        if k > self.c:
+            raise ValueError("La columna k debe ser menor que c.")
         if self.by_row == True:
             for n in elems: 
                 if col == k:
@@ -148,6 +159,8 @@ class myarray1:
         elems = self.lista 
         j = coord[0]
         k = coord[1]
+        if j > self.r or k > self.c:
+           raise ValueError("Las coordenadas j y k deben ser menores que r y c.")
         m = matriz.get_pos(j,k)
         return elems[m]
     
@@ -157,6 +170,8 @@ class myarray1:
         
         elems = self.lista
         new_object = []
+        if j > self.r:
+           raise ValueError("La fila j debe ser menor que r.")
         restar = matriz.get_row(j)
         for n in elems:
             if n not in restar:
@@ -170,6 +185,8 @@ class myarray1:
         
         elems = self.lista
         new_object = []
+        if k > self.c:
+            raise ValueError("La columna k debe ser menor que c.")
         restar = matriz.get_col(k)
         for n in elems:
             if n not in restar:
@@ -182,6 +199,8 @@ class myarray1:
         
        elems = self.lista
        listanueva = self.lista.copy()
+       if j > self.r or k > self.r:
+           raise ValueError("Las filas j y k deben ser menores que r.")
        if self.by_row == True:
            for n in range (self.c):
                listanueva[(j-1)*self.c + n] = elems[(k-1)*self.c + n]
@@ -198,7 +217,9 @@ class myarray1:
         """ Devuelve una nueva lista con las columnas l y m intercambiadas """
         
         elems = self.lista 
-        nuevascols = self.lista.copy()  
+        nuevascols = self.lista.copy() 
+        if l > self.c or m > self.c:
+           raise ValueError("Las columnas l y m deben ser menores que c.")
         if self.by_row == True:
             for n in range (self.r):
                c1 = n * self.c + (l-1)
@@ -219,15 +240,13 @@ class myarray1:
         """ Devuelve un nuevo objeto de la clase multiplicando la fila j por el 
         número x, el resto de los elementos quedan igual"""
         
-        nuevalista = []
-        fila = matriz.get_row(j)
-        for n in fila:
-            nuevalista.append (n*x)
+        newlist = self.lista.copy()
+        if j > self.r:
+           raise ValueError("La fila j debe ser menor que r.")
+        for i in range(self.c):
+            newlist[(j - 1) * self.c + i] *= x
             
-        listr = matriz.del_row(j).lista
-        listr.insert((j-1)*self.c,nuevalista)
-            
-        return myarray1(listr,self.r,self.c,self.by_row)
+        return newlist
     
     def scale_cols (self,k,y):
         
@@ -236,6 +255,8 @@ class myarray1:
         
         elems = self.lista
         nuevalista = []
+        if k > self.c:
+           raise ValueError("La columna k debe ser menor que c.")
         if self.by_row == True:
             for n in range (len(elems)):
                 if n % self.c == k-1:
@@ -294,49 +315,315 @@ class myarray1:
         espejo"""
     
         elems = self.lista
+        fliprows = []
         if self.by_row == True:
             first_row = elems[:self.c]
             last_row = elems[(self.r - 1) * self.c:self.r * self.c]
-            new_flist = last_row + elems[self.c:self.c * (self.r - 1)] + first_row
+            salida = last_row + elems[self.c:self.c * (self.r - 1)] + first_row
         if self.by_row == False:
-            pass 
+            for c in range(self.r):
+                col = elems [c::self.c]
+                fliprows.extend(col[::-1])
+                salida = fliprows
+        return salida 
+    
+    def det (self):
         
-        return new_flist
-      
+        """ Calcula el determinante de una matriz cuadrada"""
+        
+        elems = self.lista 
+        if self.r != self.c:
+            raise ValueError("La matriz debe ser cuadrada.")
+        
+        if self.r == 1:
+            return elems[0]
+        
+        elif self.r == 2:
+            return elems[0]*elems[3] - elems[1]*elems[2]
+        
+        else:
+        
+            det = 0
+            signo = 1
+            
+            for j in range(self.c):
+                submatriz = []
+                for i in range(1, self.r):
+                    for k in range(self.c):
+                        if k != j:
+                            submatriz.append(elems[i*self.c + k])
+                submatriz_det = myarray1(submatriz, self.r-1, self.c-1,self.by_row).det()
+                det += elems[j] * submatriz_det * signo
+                signo = -signo
+
+            return det
+    
+    def __add__ (self, other):
+        
+        """ Redefine la operación de la suma para poder utilizarla dentro de la clase"""
+        
+        if isinstance (other,myarray1):
+            if self.r != other.r or self.c != other.c:
+                raise ValueError ("Las matrices deben ser del mismo tamaño para sumarlas")
+            suma = []
+            for fila in range (self.r):
+                for col in range (self.c):
+                    pos1 = self.get_pos(fila+1,col+1)
+                    pos2 = self.get_pos(fila+1,col+1)
+                    suma.append(self.lista[pos1]+ other.lista[pos2])
+        if isinstance (other,int):
+            suma = []
+            for fila in range (self.r):
+                for col in range (self.c):
+                    pos = self.get_pos(fila+1,col+1)
+                    suma.append(self.lista[pos]+other)
+                
+        return suma
+    
+    def __radd__ (self,num):
+        
+        """ Redefine la operación de la suma para poder utilizarla dentro de la clase, 
+        pero para que se pueda operar en cualquier orden"""
+        
+        if isinstance (num,int):
+            return self.__add__(num)
+        
+
+    def __sub__ (self, other):
+        
+        """ Redefine la operación de la resta para poder utilizarla dentro de la clase"""
+        
+        if isinstance (other,myarray1):
+            if self.r != other.r or self.c != other.c:
+                raise ValueError ("Las matrices deben ser del mismo tamaño para restarlas")
+            resta = []
+            for fila in range (self.r):
+                for col in range (self.c):
+                    pos1 = self.get_pos(fila+1,col+1)
+                    pos2 = self.get_pos(fila+1,col+1)
+                    resta.append(self.lista[pos1] - other.lista[pos2])
+        if isinstance (other,int):
+            resta = []
+            for fila in range (self.r):
+                for col in range (self.c):
+                    pos = self.get_pos(fila+1,col+1)
+                    resta.append(self.lista[pos] - other)
+                
+        return resta
+    
+    def __rsub__ (self,num):
+        
+        """ Redefine la operación de la resta para poder utilizarla dentro de la clase, 
+        pero para que se pueda operar en cualquier orden"""
+        
+        if isinstance (num,int):
+            return self.__sub__(num)
+    
+    def __mul__ (self,num): 
+        
+        """ Redefine la operación de la multiplicación para poder utilizarla dentro de la clase"""
+        
+        if isinstance (num,int):
+            mult = []
+            for i in range (len(self.lista)):
+                mult.append(self.lista[i]*num)
+        elif isinstance (num,myarray1):
+            mult = []
+            for i in range(len(self.lista)):
+                mult.append(self.lista[i] * num.lista[i])
           
-if __name__ == "__main__":
-    matriz = myarray1 ([1,2,3,4,5,6,7,8,9],3,3,True)
-    pos = matriz.get_pos(2,3)
-    coord = matriz.get_coords (1)
-    lista_switch = matriz.switch()
-    fila = matriz.get_row (2)
-    columna = matriz.get_col(2)
-    m = matriz.get_elem((2,1))
-    delrow = matriz.del_row(1)
-    delcol = matriz.del_col (1) 
-    swaprows = matriz.swap_rows (1,3)
-    swapcols = matriz.swap_cols(1,3)
-    scalerow = matriz.scale_rows(2, 2)
-    scalecol = matriz.scale_cols(1, 2)
-    transp = matriz.transpose ()
-    flipcol = matriz.flip_cols()
-    fliprows = matriz.flip_rows()  
-
-#%% 
-"""Para multiplicar las matrices, las columnas de A tienen q ser igual a las filas de B
-   Resultado = Filas de A por las columnas de B. 
-   getrow (i), getcol (j)
-   [l1[n]*l2[n] for n in range (self.c)] sumar 
-   
-   class eye (myarray1) es una subclase. Hereda los atributos de myarray1. 
-   
-   def __init__ (self,n):
-       self.elems = [0] x (n**2)
-       poner unos en la posicion 1,1 2,2 3,3 4,4
-       self.r = n
-       self.c = n
-
-   premultiplicar es rxr y posmultiplicar es cxc
+        return mult
+    
+    def __rmul__ (self, num):
         
-"""
+        """ Redefine la operación de la multiplicación para poder utilizarla dentro de la clase, 
+        pero para que se pueda operar en cualquier orden"""
+        
+        return self.__mul__(num)
+    
+    def __matmul__ (self,other):
+        
+        """ Redefine la operación de la multiplicación para poder multiplicar matrices 
+        dentro de la clase"""
+        
+        if self.c != other.r:
+            raise ValueError ("La cantidad de columnas de la primer matriz debe ser igual a la cantidad de filas de la segunda matriz")
+        resultado = []
+        for i in range (1, self.r + 1):
+            fila = []
+            for j in range (1, other.c + 1):
+                producto = sum(x*y for x,y in zip (self.get_row(i), other.get_col(j)))
+                fila.append(producto)
+            resultado.extend(fila)
+            
+        return myarray1 (resultado, self.r, other.c, self.by_row)
+    
+    def __pow__ (self,exp):
+        
+        """ Redefine la operación de potenciación para poder utilizarla dentro de la clase"""
+        
+        if self.r != self.c: 
+            raise ValueError ("La matriz tiene que ser cuadrada")
+        if exp < 0:
+            raise ValueError ("La potencia tiene que ser un número entero positivo")
+        if exp == 1:
+            resultado = myarray1 (self.lista,self.r,self.c,self.by_row)
+        else: 
+            resultado = myarray1 (self.lista,self.r,self.c,self.by_row)
+            for i in range (exp-1):
+                resultado = resultado.__matmul__(self)
+        return resultado.lista
+    
+    def ident_matrix(self):
+        
+        """Inicializar una lista vacía para almacenar los elementos de la matriz"""
+        
+        identidad = []
+        
+        """Colocar unos en la diagonal principal y ceros en las demás posiciones"""
+        
+        for i in range(self.r):
+            for j in range(self.c):
+                if i == j:
+                    identidad.append(1)
+                else:
+                    identidad.append(0)
+                
+        return identidad
+    
+    def ident_swaprow (self, j, k):
+        
+        """Devuelve una nueva lista con las filas j y k intercambiadas."""
+        
+        listanueva = identity.copy()
+        if j > self.r or k > self.r:
+            raise ValueError("Las filas j y k deben ser menores que r.")
+        if self.by_row == True:
+            for n in range (self.c):
+                listanueva[(j-1)*self.c + n] = identity[(k-1)*self.c + n]
+                listanueva[(k-1)*self.c + n] = identity[(j-1)*self.c + n]
+        if self.by_row == False: 
+            for n in range (self.r):  
+                listanueva[n*self.c + (j-1)] = identity[n*self.c + (k-1)]
+                listanueva[n*self.c + (k-1)] = identity[n*self.c + (j-1)]
+                
+        return myarray1(listanueva,self.r,self.c,self.by_row)
+                
+    def ident_swapcol (self, l, m):
+        
+        """ Devuelve una nueva lista con las columnas l y m intercambiadas """
+        
+        nuevascols = identity.copy() 
+        if l > self.c or m > self.c:
+           raise ValueError("Las columnas l y m deben ser menores que c.")
+        if self.by_row == True:
+            for n in range (self.r):
+               c1 = n * self.c + (l-1)
+               c2 = n * self.c + (m-1)
+               nuevascols[c1] = identity[c2]
+               nuevascols[c2] = identity[c1]
+        if self.by_row == False:
+            for n in range(self.c):
+               c1 = (l-1)* self.r + n
+               c2 = (m-1) * self.r + n
+               nuevascols[c1], nuevascols[c2] = identity[c2], identity[c1]
+
+        return myarray1(nuevascols,self.r,self.c,self.by_row)
+    
+    def ident_delrow (self,j):
+        
+        """Devuelve una nueva lista sin la fila j de la matriz original."""
+        
+        new_object = []
+        if j > self.r:
+           raise ValueError("La fila j debe ser menor que r.")
+        for i in range (self.r):
+            if i != (j-1):
+                for k in range (self.c):
+                    new_object.append(identity[i*self.c+k])
+      
+        return myarray1(new_object,self.r-1, self.c,self.by_row)
+
+    def ident_delcol (self,k):
+        
+        """Devuelve una nueva lista sin la columna k de la matriz original."""
+        
+        new_object = []
+        if k > self.c:
+            raise ValueError("La columna k debe ser menor que c.")
+        for i in range (self.r):
+            for j in range (self.c):
+                if j != (k-1):
+                    new_object.append(identity[i*self.c+j])
+                    
+        return myarray1(new_object,self.r, self.c-1,self.by_row)
+        
+        
+if __name__ == "__main__":
+     matriz = myarray1([1,2,3,4,5,6,7,8,9],3,3,True)
+     print(f'La matriz es: {matriz}')
+     pos = matriz.get_pos(1,2)
+     print(f'La posición del elemento en la lista es:\t\n{pos}') 
+     coord = matriz.get_coords (1)
+     print(f'Las coordenadas de la posición dada son:\t\n{coord}') 
+     lista_switch = matriz.switch()
+     print(f'La lista intercambiada queda:\t\n{lista_switch.lista}') 
+     fila = matriz.get_row(2)
+     print(f'La fila es:\t\n{fila}') 
+     columna = matriz.get_col(2)
+     print(f'La columna es:\t\n{columna}') 
+     m = matriz.get_elem((2,1))
+     print(f'El elemento es:\t\n{m}') 
+     delrow = matriz.del_row(1)
+     print(f'La nueva matriz es:\t\n{delrow.lista}') 
+     delcol = matriz.del_col(1)
+     print(f'La nueva matriz es:\t\n{delcol.lista}') 
+     swaprows = matriz.swap_rows (1,2)
+     print(f'La matriz con las filas intercambiadas queda:\t\n{swaprows.lista}') 
+     swapcols = matriz.swap_cols(1,2)
+     print(f'La matriz con las columnas intercambiadas queda:\t\n{swapcols.lista}') 
+     scalerow = matriz.scale_rows(2, 2)
+     print(f'Las filas multiplicadas por un escalar quedan:\t\n{scalerow}') 
+     scalecol = matriz.scale_cols(1, 2)
+     print(f'Las columnas mutiplicadas por un escalar quedan:\t\n{scalecol}') 
+     transp = matriz.transpose()
+     print(f'La lista transpuesta queda:\t\n{transp}') 
+     flipcol = matriz.flip_cols()
+     print(f'La lista con las columnas intercambiadas queda:\t\n{flipcol}') 
+     fliprows = matriz.flip_rows()
+     print(f'La lista con las filas intercambiadas queda:\t\n{fliprows}') 
+     det = matriz.det()
+     print(f'El determinante es:\t\n{det}') 
+     matriz1 = myarray1([1,2,3,4],2,2,True)
+     matriz2 = myarray1([5,6,7,8],2,2,True)
+     identity = matriz.ident_matrix()
+     print (f'La identidad de la matriz es:\t\n{identity}')
+     swaprow_ident = matriz.ident_swaprow(1,3)
+     swapcol_ident = matriz.ident_swapcol(2,3)
+     delrow_ident = matriz.ident_delrow(2)
+     delcol_ident = matriz.ident_delcol(1)
+     mul_swaprows = (swaprow_ident@matriz).lista
+     print (f'Intercambiando las filas de la identidad, la matriz es:\t\n{mul_swaprows}')
+     mul_swapcols = (swapcol_ident@matriz).lista
+     print (f'Intercambiando las columnas de la identidad, la matriz es:\t\n{mul_swapcols}')
+     mul_delrow = (delrow_ident@matriz).lista
+     print (f'Eliminando una fila de la identidad, la matriz es:\t\n{mul_delrow}')
+     mul_delcol = (matriz@delcol_ident).lista
+     print (f'Eliminando una columna de la identidad, la matriz es:\t\n{mul_delcol}')
+     suma = matriz1 + matriz2
+     print(f'La suma de matrices es:\t\n{suma}') 
+     nsuma = 2 + matriz1
+     print(f'La suma de un número a la matriz es:\t\n{nsuma}') 
+     resta = matriz1 - matriz2
+     print(f'La resta de matrices es:\t\n{resta}') 
+     nresta = 1 - matriz2
+     print(f'La resta de un número a la matriz es:\t\n{nresta}') 
+     nmul = matriz1*2
+     print(f'La mutiplicación de la matriz por un escalar es:\t\n{nmul}') 
+     matmul = (matriz1@matriz2).lista
+     print(f'La multiplicación de matrices es:\t\n{matmul}') 
+     power = matriz1**3
+     print(f'La potenciación de la matriz es:\t\n{power}') 
+     
+
 
